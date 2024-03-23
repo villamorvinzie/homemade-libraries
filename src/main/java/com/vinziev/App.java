204b61896854b8fccf9d2e887ac0a82ee3d431c9
Vinzie Villamor <villamorvinzie@gmail.com>
2024-03-23 10:48:08 +0800

    feat: multi-module project

---
+++ src/main/java/com/vinziev/App.java
@@ -0,0 +1,13 @@
+package com.vinziev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
